cc6fb7316b76cd4984555f97e2546e0f9acde403
Jie <linsijia628@gmail.com>
2023-04-20 12:05:58 +0800

    fix: update harpoon event

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,11 +1,10 @@
 return {
     "ThePrimeagen/harpoon",
     enabled = true,
-    cmd = { "HydraHarpoon" },
+	event = "VeryLazy",
     config = function()
         require("harpoon").setup({
             global_settings = {
-                -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
                 save_on_toggle = false,
                 save_on_change = true,
                 -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
