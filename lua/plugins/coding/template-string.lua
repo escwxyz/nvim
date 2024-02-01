ba09e9b618ac395edc94fc759b16f0d082395469
Jie <linsijia628@gmail.com>
2024-02-01 20:19:21 +0800

    chore: plugin updates

---
+++ lua/plugins/coding/template-string.lua
@@ -0,0 +1,21 @@
+return {
+  "axelvc/template-string.nvim",
+  ft = {
+    "javascript",
+    "typescript",
+    "typescriptreact",
+    "javascriptreact",
+    "html",
+    -- I don't write vue
+    -- "vue"
+  },
+  opts = {
+    filetypes = {
+      "javascript",
+      "typescript",
+      "typescriptreact",
+      "javascriptreact",
+      "html",
+    },
+  },
+}
