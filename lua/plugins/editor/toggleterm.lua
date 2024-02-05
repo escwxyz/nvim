e67a95e29ed81485822288202f9bdc9817cea7de
Jie <linsijia628@gmail.com>
2024-02-05 08:47:17 +0800

    chore: adjust toggleterm

--- lua/plugins/editor/toggleterm.lua
+++ lua/plugins/editor/toggleterm.lua
@@ -1,52 +1,44 @@
+-- TODO: guicursor sometimes disappears in terminal
+-- currently not support multiple terminals in different layouts
+-- see: https://github.com/akinsho/toggleterm.nvim/issues/533
+-- maybe https://github.com/NvChad/nvterm is worth to check out
 return {
   "akinsho/toggleterm.nvim",
-  cmd = "ToggleTerm",
-  keys = function()
-    local Terminal = require("toggleterm.terminal").Terminal
+  keys = {
+    {
+      "<leader>gl",
+      function()
+        local lazygit = require("toggleterm.terminal").Terminal:new({
+          cmd = "lazygit",
+          direction = "float",
+          hidden = true,
+          close_on_exit = true,
+          on_open = function(term)
+            vim.cmd("startinsert!")
+            vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+          end,
+          on_close = function(_)
+            vim.cmd("startinsert!")
+          end,
+        })
 
-    local lazygit = Terminal:new({
-      cmd = "lazygit",
-      direction = "float",
-      close_on_exit = true,
-      on_open = function(term)
-        vim.cmd("startinsert!")
-        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+        lazygit:toggle()
       end,
-      on_close = function(_)
-        vim.cmd("startinsert!")
-      end,
-    })
-
-    return {
-      {
-        "<leader>gl",
-        function()
-          lazygit:toggle()
-        end,
-        {
-          desc = "[L]azy[G]it",
-        },
-      },
-      -- {
-      --   "<C-t>",
-      --   "<cmd>ToggleTermToggleAll<CR>",
-      --   desc = "Toggle all terminals",
-      -- },
-
-      -- { "<C-\\>h", "<cmd>ToggleTerm direction=horizontal<CR>", desc = "Toggle horizontal terminal" },
-
-      -- { "<C-\\>v", "<cmd>ToggleTerm size=80 direction=vertical<CR>", desc = "Toggle vertical terminal" },
-
-      {
-        "<C-\\>",
-        "<cmd>ToggleTerm direction=float<CR>",
-        desc = "Floating terminal",
-      },
-    }
-  end,
+      desc = "Lazygit",
+    },
+    {
+      "<C-\\>",
+      "<cmd>execute v:count . 'ToggleTerm'<CR>",
+      desc = "Toggle terminal",
+    },
+  },
   opts = {
+    direction = "horizontal",
+    start_in_insert = false,
+    insert_mappings = false,
     float_opts = {
       border = "curved",
+      title_pos = "center",
     },
   },
 }
