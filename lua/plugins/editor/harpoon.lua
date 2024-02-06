2908dbc4ae91197c73cb27d1ac35bff4724795c5
Jie <linsijia628@gmail.com>
2024-02-06 18:32:23 +0800

    chore: migrate to harpoon 2

--- lua/plugins/editor/harpoon.lua
+++ lua/plugins/editor/harpoon.lua
@@ -1,40 +1,65 @@
 return {
   "ThePrimeagen/harpoon",
-  -- TODO: adjust keymaps
-  -- keys = {
-  --   {
-  --     "<leader>a",
-  --     function()
-  --       require("harpoon.mark").add_file()
-  --       vim.notify("Mark added")
-  --     end,
-  --     desc = "[Harpoon] Add mark",
-  --   },
-  --   {
-  --     "<leader>m",
-  --     "<cmd>Telescope harpoon marks<CR>",
-  --     desc = "[Harpoon] Marks",
-  --   },
-  --   {
-  --     "<leader>]",
-  --     function()
-  --       require("harpoon.ui").nav_next()
-  --     end,
-  --     desc = "[Harpoon] Next mark",
-  --   },
-  --   {
-  --     "<leader>[",
-  --     function()
-  --       require("harpoon.ui").nav_prev()
-  --     end,
-  --     desc = "[Harpoon] Prev mark",
-  --   },
-  -- },
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = function()
+    local harpoon = require("harpoon")
+    return {
+      {
+        "<leader>h",
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "[Harpoon] Quick menu",
+      },
+      {
+        "<leader>a",
+        function()
+          harpoon:list():append()
+        end,
+        desc = "[Harpoon] Append mark",
+      },
+      {
+        "<leader>1",
+        function()
+          harpoon:list():select(1)
+        end,
+        desc = "[Harpoon] Go to mark 1",
+      },
+      {
+        "<leader>2",
+        function()
+          harpoon:list():select(2)
+        end,
+        desc = "[Harpoon] Go to mark 2",
+      },
+      {
+        "<leader>3",
+        function()
+          harpoon:list():select(3)
+        end,
+        desc = "[Harpoon] Go to mark 3",
+      },
+      {
+        "<leader>]",
+        function()
+          harpoon:list():next()
+        end,
+        desc = "[Harpoon] Next mark",
+      },
+      {
+        "<leader>[",
+        function()
+          harpoon:list():prev()
+        end,
+        desc = "[Harpoon] Prev mark",
+      },
+    }
+  end,
   config = function()
-    require("harpoon").setup({})
-
-    -- TODO: remove telescope
-    -- require("telescope").load_extension("harpoon")
+    local harpoon = require("harpoon")
+    harpoon:setup()
+    -- TODO: check more here
+    -- see: https://github.com/ThePrimeagen/harpoon/tree/harpoon2
   end,
 }
-
