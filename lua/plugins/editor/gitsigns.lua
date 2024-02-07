de3d384c14f4fd3c1215ccf3a935f5b5730d4c4a
Jie <linsijia628@gmail.com>
2024-02-07 17:02:02 +0800

    chore: adjust gitsigns keymaps

--- lua/plugins/editor/gitsigns.lua
+++ lua/plugins/editor/gitsigns.lua
@@ -8,7 +8,7 @@ return {
       add = { text = git_status_icons.added },
       change = { text = git_status_icons.modified },
       delete = { text = git_status_icons.deleted },
-      -- TODO
+      -- TODO:
       topdelete = { text = "" },
       changedelete = { text = "▎" },
       untracked = { text = git_status_icons.untracked },
@@ -25,13 +25,13 @@ return {
       map("n", "[h", gs.prev_hunk, "Prev Hunk")
       map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
       map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-      map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-      map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-      map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-      map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-      map("n", "<leader>ghd", gs.diffthis, "Diff This")
-      map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+      map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+      map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      map("n", "<leader>gd", gs.diffthis, "Diff This")
+      map("n", "<leader>gD", function() gs.diffthis("~") end, "Diff This ~")
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
     end,
   },
